1547c921cbe338f1f90668da22dcd500f2ed6312
gerrygeko <lamorte.gerardo@gmail.com>
2020-04-23 19:37:00 +0200

    Added libraries and figuring out how to load csv

--- launcher.py
+++ launcher.py
@@ -1,3 +1,17 @@
+import pandas as pd
+import requests
+from pandas import DataFrame
+
+df = DataFrame
+url_csv_national_data = "https://github.com/pcm-dpc/COVID-19/blob/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+
+
+def load_csv():
+    #data_from_requests = requests.get(url_csv_national_data).content
+    data = pd.read_csv(url_csv_national_data)
+    #print(data)
+
+
 if __name__ == '__main__':
     print("Hello world!")
-    print("Ciao caro Bro!")
+    load_csv()
