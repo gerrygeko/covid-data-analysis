7b0aa954a9977fa0a03fcad7b046ed08f9125da1
darkkillex <mastrangelo.nicola@gmail.com>
2020-04-23 23:02:12 +0200

    Plot with subplots, grid and three control line

--- launcher.py
+++ launcher.py
@@ -1,16 +1,36 @@
 import pandas as pd
-import requests
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 from pandas import DataFrame
 
+
+
 df = DataFrame
-url_csv_national_data = "https://github.com/pcm-dpc/COVID-19/blob/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+url_csv_national_data = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
 
 
 def load_csv():
     #data_from_requests = requests.get(url_csv_national_data).content
     data = pd.read_csv(url_csv_national_data)
-    #print(data)
-
+    print(data.head())
+    df = DataFrame(data, columns=['data', 'nuovi_positivi', 'deceduti', 'dimessi_guariti'])
+    #df['data'] = pd.to_datetime(df['data'])
+    #df['data'] = df['data'].dt.strftime('%d-%m')
+    fig, ax = plt.subplots(figsize=(11, 5))
+    t = pd.to_datetime(df['data'])
+    ax.plot(t, df['nuovi_positivi'], label='Nuovi Positivi')
+    ax.plot(t, df['deceduti'], label='Deceduti')
+    ax.plot(t, df['dimessi_guariti'], label='Dimessi/Guariti')
+    ax.set_xlabel('Data')
+    ax.set_ylabel('N° of People')
+    ax.set_title('COVID-19')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%d-%b"))
+    ax.xaxis.set_minor_formatter(mdates.DateFormatter("%d-%b"))
+    ax.grid(True)
+    ax.legend()
+    plt.show()
+    #ax.tick_params(labelsize=10)
 
 if __name__ == '__main__':
     print("Hello world!")
