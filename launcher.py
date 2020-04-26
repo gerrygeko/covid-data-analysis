aef13dd33ef5d30989fbc1b5e6855c217c1e9112
gerrygeko <lamorte.gerardo@gmail.com>
2020-04-26 18:39:50 +0200

    Adjusted the grouped bar chart to show correctly dates and also set a fixed margin for subplots compared to the main plot

--- launcher.py
+++ launcher.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from matplotlib.dates import date2num
 
 """ Column names for National Data
 ['data' 'stato' 'ricoverati_con_sintomi' 'terapia_intensiva'
@@ -42,7 +41,7 @@ def create_time_plot_relative_numbers(df, axis):
     axis.plot(df_formatted, df['nuovi_positivi'], label='Nuovi Positivi')
     axis.plot(df_formatted, df['dimessi_giornalieri'], label='Dimessi Giornalieri')
 
-    set_labels_and_title_for_axis(axis, x_name='Time', y_name='N° of People')
+    set_labels_and_title_for_axis(axis, y_name='N° of People')
 
     axis.minorticks_on()
     # Set intervals and format on the x axis
@@ -60,16 +59,40 @@ def create_time_plot_relative_numbers(df, axis):
 def create_bar_graph_latest_number(df, axis):
     df_last_days = df.tail(7)
     df_formatted = pd.to_datetime(df_last_days['data'])
-    x = date2num(df_formatted)
+    # Convert the date to numbers so you can use floats to move around the bars and group them one next to the other
+    date2num = mdates.date2num(df_formatted)
+
     width = 0.2  # size of the bar
-    axis.bar(x, df_last_days['nuovi_positivi'], width, color='b', label='Positive Test per day')
-    axis.bar(x + 0.2, df_last_days['tamponi_giornalieri'], width, color='r', label='Testing per day')
+    rects1 = axis.bar(date2num + 0.2, df_last_days['nuovi_positivi'], width, color='b', label='Positive Tests')
+    rects2 = axis.bar(date2num + 0.4, df_last_days['tamponi_giornalieri'], width, color='r', label='Tests per day')
+
+    autolabel_bars(rects1, axis)
+    autolabel_bars(rects2, axis)
+
+    set_labels_and_title_for_axis(axis, y_name='N° of People')
+
+    dates = mdates.num2date(date2num)
+    axis.set_xticklabels(dates)
+
+    axis.xaxis.set_major_locator(mdates.DayLocator(interval=1))
     axis.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b'))
-    axis.xaxis_date()
-    axis.autoscale(tight=True)
-    axis.set_xticklabels(df_formatted)
-    plt.legend(loc='upper left', fontsize=16)
-    plt.show()
+
+    # Customize the major grid
+    axis.grid(which='major', linestyle='-', linewidth='0.5', color='grey')
+
+    axis.legend(loc=2, prop={'size': 9})
+
+
+def autolabel_bars(rects, axis):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        axis.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, -2),  # -2 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
 
 def set_labels_and_title_for_axis(axis, x_name=None, y_name=None, title=None):
     if x_name is not None: axis.set_xlabel(x_name)
@@ -78,18 +101,16 @@ def set_labels_and_title_for_axis(axis, x_name=None, y_name=None, title=None):
 
 
 def configure_mainplot_with_subplots():
-    figure, axis = plt.subplots(2, 2, sharex=True)
+    figure, axis = plt.subplots(2, 2)
+    # Set distance of the subplots using margin distance with main plot area
+    figure.subplots_adjust(left=0.08, right=0.96, bottom=0.1, top=0.9)
     figure.suptitle('COVID-19')
     # Set size of the window
-    figure.set_figheight(7)
-    figure.set_figwidth(11)
+    figure.set_figheight(8)
+    figure.set_figwidth(15)
     # Set position of the window in the screen
     manager = plt.get_current_fig_manager()
-    manager.window.wm_geometry('+450+100')
-    ########################################
-    figure.subplots_adjust(bottom=0.3)
-    figure.text(0.1, 0.10, 'Esempio stampa')
-    ###########################################
+    manager.window.wm_geometry('+250+40')
     return figure, axis
 
 
@@ -102,6 +123,7 @@ def calculate_and_add_daily_variance_of_dimessi(national_data):
         else:
             list_dimessi_giornalieri.append(list_dimessi[i] - list_dimessi[i - 1])
     national_data['dimessi_giornalieri'] = list_dimessi_giornalieri
+
 
 def calculate_and_add_daily_variance_of_tamponi(national_data):
     list_tamponi = national_data['tamponi'].values.tolist()
@@ -118,10 +140,11 @@ def run_application():
     national_data = load_csv(url_csv_national_data)
     calculate_and_add_daily_variance_of_dimessi(national_data)
     calculate_and_add_daily_variance_of_tamponi(national_data)
-    figure, ((axis_1, axis_2),(axis_2, axis_3)) = configure_mainplot_with_subplots()
+    figure, ((axis_1, axis_2), (axis_3, axis_4)) = configure_mainplot_with_subplots()
     create_time_plot_total_numbers(national_data, axis_1)
-    create_time_plot_relative_numbers(national_data, axis_2)
-    create_bar_graph_latest_number(national_data, axis_3)
+    create_time_plot_relative_numbers(national_data, axis_3)
+    create_bar_graph_latest_number(national_data, axis_2)
+    create_bar_graph_latest_number(national_data, axis_4)
     plt.show()
 
 
