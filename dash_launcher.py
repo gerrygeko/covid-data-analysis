c1f380385700b1958bab50a7d9a1f836d828b572
gerrygeko <lamorte.gerardo@gmail.com>
2020-05-03 01:34:43 +0200

    Rearrange code around

--- dash_launcher.py
+++ dash_launcher.py
@@ -5,32 +5,18 @@ import dash_core_components as dcc
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 
+app = dash.Dash(__name__)
 url_csv_regional_data = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
 url_csv_italy_data = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
 
 
 def load_csv(url):
-    data_loaded = pd.read_csv(url, parse_dates=True)
+    data_loaded = pd.read_csv(url, index_col=[0], parse_dates=['data'])
     return data_loaded
 
 
-df_italy_region = load_csv(url_csv_regional_data)
-df_italy_region.index = pd.to_datetime(df_italy_region['data'])
-
-df_italy = load_csv(url_csv_italy_data)
-df_italy.index = pd.to_datetime(df_italy['data'])
-
-# Initialise the app
-app = dash.Dash(__name__)
-app.config.suppress_callback_exceptions = True
-
-
-# When we generate dynamic components from callbacks (as in your case),
-# Dash has no way of knowing which IDs you are generating.
-# After all, the IDs could be totally dynamic!
-# Dash assumes that you are making a typo by assigning a callback to an ID that doesn’t exist at app start but
-# allows you to manually correct this assumption by setting app.config['suppress_callback_exceptions']=True.
-# This forces you to acknowledge that Dash will not protect you against ID exceptions.
+df_national_data = load_csv(url_csv_regional_data)
+df_regional_data = load_csv(url_csv_italy_data)
 
 
 def get_options(list_value):
@@ -41,21 +27,22 @@ def get_options(list_value):
     return dict_list
 
 
-def create_figure_national_plot_linear():
-    df_sub = df_italy
-    time_set = df_sub['data']
+def create_figure_plot(y_is_log=False):
+    y_axis_type = "log" if y_is_log else "linear"
+    df_sub = df_regional_data
+    time_set = df_sub.index
     tot_cases = df_sub['totale_casi']
     tot_positive = df_sub['totale_positivi']
-    symptomatics_patients = df_sub['ricoverati_con_sintomi']
+    symptomatic_patients = df_sub['ricoverati_con_sintomi']
     intensive_care = df_sub['terapia_intensiva']
-    scatter1 = go.Scatter(x=time_set, y=tot_cases, mode='lines+markers', opacity=0.7, name="Total cases",
-                         textposition='bottom center')
-    scatter2 = go.Scatter(x=time_set, y=tot_positive, mode='lines+markers', opacity=0.7, name="Currently positive",
-                          textposition='bottom center')
-    scatter3 = go.Scatter(x=time_set, y=symptomatics_patients, mode='lines+markers', opacity=0.7, name="Symptomatics patients",
-                         textposition='bottom center')
-    scatter4 = go.Scatter(x=time_set, y=intensive_care, mode='lines+markers', opacity=0.7, name="Intensive care",
-                          textposition='bottom center')
+    scatter1 = go.Scatter(x=time_set, y=tot_cases, mode='lines+markers', opacity=0.7,
+                          name="Total cases", textposition='bottom center')
+    scatter3 = go.Scatter(x=time_set, y=symptomatic_patients, mode='lines+markers', opacity=0.7,
+                          name="Symptomatics patients", textposition='bottom center')
+    scatter2 = go.Scatter(x=time_set, y=tot_positive, mode='lines+markers', opacity=0.7,
+                          name="Currently positive", textposition='bottom center')
+    scatter4 = go.Scatter(x=time_set, y=intensive_care, mode='lines+markers', opacity=0.7,
+                          name="Intensive care", textposition='bottom center')
 
     figure = {'data': [scatter1, scatter2, scatter3, scatter4],
               'layout': go.Layout(
@@ -68,108 +55,14 @@ def create_figure_national_plot_linear():
                   autosize=True,
                   title={'text': 'Linear Scale', 'font': {'color': 'white'}, 'x': 0.5},
                   xaxis={'range': [df_sub.index.min(), df_sub.index.max()]},
+                  yaxis_type=y_axis_type
               ),
               }
     return figure
-
-
-def create_figure_national_plot_log():
-    df_sub = df_italy
-    time_set = df_sub['data']
-    tot_cases = df_sub['totale_casi']
-    tot_positive = df_sub['totale_positivi']
-    symptomatics_patients = df_sub['ricoverati_con_sintomi']
-    intensive_care = df_sub['terapia_intensiva']
-    scatter1 = go.Scatter(x=time_set, y=tot_cases, mode='lines+markers', opacity=0.7, name="Total cases",
-                          textposition='bottom center')
-    scatter2 = go.Scatter(x=time_set, y=tot_positive, mode='lines+markers', opacity=0.7, name="Currently positive",
-                          textposition='bottom center')
-    scatter3 = go.Scatter(x=time_set, y=symptomatics_patients, mode='lines+markers', opacity=0.7,
-                          name="Symptomatics patients",
-                          textposition='bottom center')
-    scatter4 = go.Scatter(x=time_set, y=intensive_care, mode='lines+markers', opacity=0.7, name="Intensive care",
-                          textposition='bottom center')
-    figure = {'data': [scatter1, scatter2, scatter3, scatter4],
-              'layout': go.Layout(
-                  colorway=["#ffff00", '#ff0000', '#adff2f', '#f0ffff', '#00bfff', '#ffa500'],
-                  template='plotly_dark',
-                  paper_bgcolor='rgba(0, 0, 0, 0)',
-                  plot_bgcolor='rgba(0, 0, 0, 0)',
-                  margin={'b': 15},
-                  hovermode='x',
-                  autosize=True,
-                  title={'text': 'Logarithmic Scale', 'font': {'color': 'white'}, 'x': 0.5},
-                  xaxis={'range': [df_sub.index.min(), df_sub.index.max()]},
-                  yaxis_type="log"
-              ),
-              }
-    return figure
-
-
-# Define the web app
-app.layout = html.Div(
-    children=[
-        html.Div(className='row',
-                 children=[
-                     html.Div(className='four columns div-user-controls',
-                              children=[
-                                  html.H2('COVID-19:ITALY'),
-                                  html.P('by Gellex (Geko + Killex)'),
-                                  html.P('Visualising time series with Plotly - Dash.'),
-                                  html.P('Select one or more regions from the dropdown:'),
-                                  html.Div(
-                                      className='div-for-dropdown',
-                                      children=[
-                                          dcc.Dropdown(id='regionselector',
-                                                       options=get_options(
-                                                           df_italy_region['denominazione_regione'].unique()),
-                                                       multi=True,
-                                                       value=[
-                                                           df_italy_region['denominazione_regione'].sort_values()[0]],
-                                                       style={'backgroundColor': '#1E1E1E'},
-                                                       className='regionselector'
-                                                       ),
-                                      ],
-
-                                      style={'color': '#1E1E1E'})
-                              ]
-                              ),
-
-                     html.Div(className='eight columns div-for-charts bg-grey',
-                              children=[
-                                  html.Div([
-                                      dcc.Tabs([
-                                          dcc.Tab(label='Regional Area', children=[
-                                              dcc.Graph(id='regional_timeseries_linear',
-                                                        config={'displayModeBar': False},
-                                                        animate=True),
-                                              dcc.Graph(id='regional_timeseries_log', config={'displayModeBar': False},
-                                                        animate=True)
-
-                                          ]),
-                                          dcc.Tab(label='National Area', children=[
-                                              dcc.Graph(figure=create_figure_national_plot_linear(),
-                                                        config={'displayModeBar': False},
-                                                        animate=True),
-                                              dcc.Graph(figure=create_figure_national_plot_log(),
-                                                        config={'displayModeBar': False},
-                                                        animate=True)
-                                          ])
-                                      ], colors={
-                                          "border": "grey",
-                                          "primary": "gold",
-                                          "background": "grey"
-                                      })
-                                  ])
-                              ])
-                 ])
-    ]
-
-)
 
 
 def create_empty_figure_region_plot():
-    df_sub = df_italy_region
+    df_sub = df_national_data
     time_set = list(set(df_sub['denominazione_regione'].index.to_series().values))
     scatter = go.Scatter(x=time_set, y=[], mode='lines', opacity=0.7, name="No selection", textposition='bottom center')
 
@@ -192,24 +85,24 @@ def create_empty_figure_region_plot():
 # Callback for timeseries/region
 @app.callback(Output('regional_timeseries_linear', 'figure'), [Input('regionselector', 'value')])
 def update_graph(selected_dropdown_value):
-    trace1 = []
-    df_sub = df_italy_region
+    regions_list = []
+    df_sub = df_national_data
 
+    # if no region selected, create empty figure
     if len(selected_dropdown_value) == 0:
         figure = create_empty_figure_region_plot()
         return figure
 
     for region in selected_dropdown_value:
-        trace1.append(go.Scatter(x=df_sub[df_sub['denominazione_regione'] == region].index,
+        regions_list.append(go.Scatter(x=df_sub[df_sub['denominazione_regione'] == region].index,
                                  # https://plotly.com/python/line-charts/#line-plot-with-goscatter
                                  y=df_sub[df_sub['denominazione_regione'] == region]['nuovi_positivi'],
                                  mode='lines+markers',
                                  opacity=1,
                                  name=region,
                                  textposition='bottom center'))
-    traces = [trace1]
-    data = [val for sublist in traces for val in sublist]
-    figure = {'data': data,
+
+    figure = {'data': regions_list,
               'layout': go.Layout(
                   colorway=["#ffff00", '#ff0000', '#adff2f', '#f0ffff', '#00bfff', '#ffa500'],
                   # ["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
@@ -230,7 +123,7 @@ def update_graph(selected_dropdown_value):
 @app.callback(Output('regional_timeseries_log', 'figure'), [Input('regionselector', 'value')])
 def update_graph_log(selected_dropdown_value):
     trace1 = []
-    df_sub = df_italy_region
+    df_sub = df_national_data
 
     if len(selected_dropdown_value) == 0:
         figure = create_empty_figure_region_plot()
@@ -258,12 +151,79 @@ def update_graph_log(selected_dropdown_value):
                   autosize=True,
                   title={'text': 'Total Positives (log scale)', 'font': {'color': 'white'}, 'x': 0.5},
                   xaxis={'range': [df_sub.index.min(), df_sub.index.max()]},
-                  # xaxis_type="log",
                   yaxis_type="log"
               ),
               }
     return figure
 
 
+def app_layout():
+    # Define the web app
+    app.layout = html.Div(
+        children=[
+            html.Div(className='row',
+                     children=[
+                         html.Div(className='four columns div-user-controls',
+                                  children=[
+                                      html.H2('COVID-19:ITALY'),
+                                      html.P('by Gellex (Geko + Killex)'),
+                                      html.P('Visualising time series with Plotly - Dash.'),
+                                      html.P('Select one or more regions from the dropdown:'),
+                                      html.Div(
+                                          className='div-for-dropdown',
+                                          children=[
+                                              dcc.Dropdown(id='regionselector',
+                                                           options=get_options(
+                                                               df_national_data['denominazione_regione'].unique()),
+                                                           multi=True,
+                                                           value=[
+                                                               df_national_data['denominazione_regione'].sort_values()[
+                                                                   0]],
+                                                           style={'backgroundColor': '#1E1E1E'},
+                                                           className='regionselector'
+                                                           ),
+                                          ],
+
+                                          style={'color': '#1E1E1E'})
+                                  ]
+                                  ),
+
+                         html.Div(className='eight columns div-for-charts bg-grey',
+                                  children=[
+                                      html.Div([
+                                          dcc.Tabs([
+                                              dcc.Tab(label='Regional Area', children=[
+                                                  dcc.Graph(id='regional_timeseries_linear',
+                                                            config={'displayModeBar': False},
+                                                            animate=True),
+                                                  dcc.Graph(id='regional_timeseries_log',
+                                                            config={'displayModeBar': False},
+                                                            animate=True)
+
+                                              ]),
+                                              dcc.Tab(label='National Area', children=[
+                                                  dcc.Graph(figure=create_figure_plot(y_is_log=False),
+                                                            config={'displayModeBar': False},
+                                                            animate=True),
+                                                  dcc.Graph(figure=create_figure_plot(y_is_log=True),
+                                                            config={'displayModeBar': False},
+                                                            animate=True)
+                                              ])
+                                          ], colors={
+                                              "border": "grey",
+                                              "primary": "gold",
+                                              "background": "grey"
+                                          })
+                                      ])
+                                  ])
+                     ])
+        ]
+
+    )
+
+
 if __name__ == '__main__':
+    # Initialise the app
+    app.config.suppress_callback_exceptions = True
+    app_layout()
     app.run_server(debug=True)  # debug=True active a button in the bottom right corner of the web page
