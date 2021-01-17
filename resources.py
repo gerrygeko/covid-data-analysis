2a2ccc9e6d165e510341c087663162f9bec9d87c
darkkillex <23dottork13>
2021-01-17 17:23:24 +0100

    Replacement of "googletrans" lib with "pygoogletranslation"

--- resources.py
+++ resources.py
@@ -1,15 +1,15 @@
-from googletrans import Translator
+from pygoogletranslation import Translator
 
 import logger
 from constants import DEFAULT_LANGUAGE
 
 language_list = [{'label': 'Italiano', 'value': 'IT'},
                  {'label': 'English', 'value': 'EN'},
-                 # {'label': 'Dutch', 'value': 'NL'},
-                 # {'label': 'German', 'value': 'DE'},
-                 # {'label': 'French', 'value': 'FR'},
-                 # {'label': 'Spanish', 'value': 'ES'},
-                 # {'label': 'Portuguese', 'value': 'PT'}
+                  {'label': 'Dutch', 'value': 'NL'},
+                  {'label': 'German', 'value': 'DE'},
+                  {'label': 'French', 'value': 'FR'},
+                  {'label': 'Spanish', 'value': 'ES'},
+                  {'label': 'Portuguese', 'value': 'PT'}
                  ]
 
 
