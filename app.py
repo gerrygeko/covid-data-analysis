5749cab199e9a157d94283f7b022b6d895d2ddd4
darkkillex <23dottork13>
2021-02-17 16:32:09 +0100

    Refactoring: removed unused annotated code

--- app.py
+++ app.py
@@ -808,65 +808,6 @@ def update_vaccines_italy_cards_text(self):
     return (*total_text_values), sub_header_vaccines_italy_text, herd_immunity_date
 
 
-# @app.callback(Output('chart_vaccination_italy_phases', 'figure'), [Input("i_news", "n_intervals")])
-# def update_vaccination_italy_phases_line_chart(self):
-#     layout_vaccination_phases = copy.deepcopy(layout)
-#     df = add_percentage_vaccination_italy_phases(df_vaccines_italy_admin_summary_latest)
-#     colors = ["rgb(204, 51, 0)", "rgb(4, 74, 152)", "rgb(123, 199, 255)"]
-#     one_to_ninety = pd.Series(range(1, 90))
-#     x = df['percentage_vaccinated_population']
-#     y = df['percentage_vaccinated_population']
-#     data = [
-#         dict(
-#             type="scatter",
-#             mode="lines+markers",
-#             x=x,
-#             y=y,
-#             name="ciao",
-#             fill='tozeroy',
-#             marker=dict(color=colors[2]),
-#         )
-#     ]
-#
-#     annotations = []
-#
-#     annotation_phase_1 = {
-#         'x': 5,
-#         'y': 0,
-#         'xref': 'x',
-#         'yref': 'y',
-#         'text': load_resource('vaccine_phase_1'),
-#         'align': 'center',
-#         'ax': 0,
-#         'ay': -60,
-#         'opacity': 0,
-#     }
-#
-#     annotation_phase_2 = {
-#         'x': 15,
-#         'y': 0,
-#         'xref': 'x',
-#         'yref': 'y',
-#         'text': load_resource('vaccine_phase_2'),
-#         'align': 'center',
-#         'ax': 0,
-#         'ay': -60,
-#         'opacity': 0,
-#     }
-#     annotations.append(annotation_phase_1)
-#     annotations.append(annotation_phase_2)
-#
-#     updated_annotations = list(map(lambda ann: {**ann, 'opacity': 1.0}, annotations))
-#     layout_vaccination_phases["showlegend"] = True
-#     layout_vaccination_phases["autosize"] = True
-#     # layout_vaccination_phases["annotations"] = ANNOTATIONS
-#     layout_vaccination_phases = go.Layout(annotations=updated_annotations)
-#     figure = dict(data=data, layout=layout_vaccination_phases)
-#
-#     log.info('Updating Italian vaccinations phases')
-#     return figure
-
-
 @app.callback(Output('bar_chart_vaccination_daily_total', 'figure'), [Input("i_news", "n_intervals")])
 def update_bar_chart_vaccines_italy_daily_total(self):
     layout_administrations_by_day = copy.deepcopy(layout)
@@ -1302,13 +1243,6 @@ def calculate_date_of_herd_immunity():
     people_to_immunize = (constants.ITALIAN_POPULATION * 70) / 100
     days_to_her_immunity = round((people_to_immunize - last_update_vaccinated_people)/last_update_rolling_avg, 0)
     date_herd_immunity = (first_useful_date + timedelta(days=days_to_her_immunity)).strftime('%d/%m/%Y')
-    # pd.set_option("display.max_rows", None, "display.max_columns", None)
-    # print(df)
-    # print(last_update_vaccinated_people, '', last_update_rolling_avg)
-    # print('First useful date', first_useful_date)
-    # print('People to immunize:', people_to_immunize)
-    # print('The days to immunity are:', days_to_her_immunity)
-    # print('The herd immunity date will be:', date_herd_immunity)
     return date_herd_immunity
 
 
@@ -1349,8 +1283,6 @@ def load_regional_data():
         df_regional_data['pressure_ICU'] = round(((df_regional_data['terapia_intensiva'] /
                                                    df_regional_data['available_ICU']) * 100), 2)
         df_rate_regional = load_region_rate_data_frame(df_regional_data)
-        # df_regional_data['available_ICU'] = list(italy_ICU.values())
-        # print(df_regional_data[['denominazione_regione','available_ICU']].tail(42))
         date_last_update_regional = get_content_date_last_download_data(constants.URL_CSV_REGIONAL_DATA)
         log.info(f"Old Content-length: {last_update_content_regional_data} bytes")
         log.info(f"New Content-length: {current_update_content_regional_data} bytes")
