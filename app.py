d5a2cb879dddc8f5d58290835af4ec7f3da69176
darkkillex <mastrangelo.nicola@gmail.com>
2022-01-07 16:02:26 +0100

    Solved an unsupported locale setting issue on macOS

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ from utils import is_debug_mode_enabled, layout, load_csv_from_file, load_csv
 
 app_start_time = time.time()
 threading.current_thread().name = "main-thread"
-locale.setlocale(locale.LC_ALL, 'it_IT.utf8')
+#locale.setlocale(locale.LC_ALL, 'it_IT.utf8')
+curr = locale.getdefaultlocale()
+locale.setlocale(locale.LC_ALL, curr)
 logger.initialize_logger()
 log = logger.get_logger()
 debug_mode_enabled = is_debug_mode_enabled()
