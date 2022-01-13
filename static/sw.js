e26a4fb2e525e30ae1dc9a2b0a043f2e54950c6b
darkkillex <mastrangelo.nicola@gmail.com>
2022-01-13 20:09:09 +0100

    Added the Service Worker (script for the Installation phase)

---
+++ static/sw.js
@@ -0,0 +1,16 @@
+var CACHE_NAME = 'offline-coronavirus-data';
+var urlsToCache = [
+  '/assets/styles.css',
+  '/assets/s1.css',
+  '/static/register-sw.js'
+];
+self.addEventListener('install', function(event) {
+  // install files needed offline
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
